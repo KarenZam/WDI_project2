b1901459c83e53668eef47c81c375aff85b35768
Karen <chleozam@gmail.com>
2013-10-22 12:33:20 +0800

    install complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WDIProject2::Application.config.secret_key_base = 'ef66ba6a7f7027ad45d5f3b8adc5848829656017797d6d16dc34872e0a5aecdab934a904e8318891c4d866e99b64975b8dd76c5d9043ba9fdddce69a1d0f888a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WDIProject2::Application.config.session_store :cookie_store, key: '_WDI_project2_session'
